114fca56116f4a3a3dd7eb2a00a20b403b934a7b
jgersain <jhersein@gmail.com>
2019-10-16 18:45:24 -0500

    create and add 'not found url page'

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from '../Layout';
 import Home from '../../pages/Home';
+import NotFound from '../../pages/NotFound';
 
 const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route component={ NotFound } />
       </Switch>
     </Layout>
   </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles/NotFound.scss'
+
+const NotFound = () => <div className="background"/>
+
+export default NotFound;
