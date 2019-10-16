9360cfb030a3122d7299b62f7cd5619bc79f7442
jgersain <jhersein@gmail.com>
2019-10-16 18:52:49 -0500

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer } from 'mdbreact';
+// TODO import logo here
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <MDBNavbar color="deep-purple lighten-1" fixed="top" dark expand="md" scrolling transparent>
+          <MDBContainer>
+            <MDBNavbarBrand href="/">
+              <strong>LOGO</strong>
+            </MDBNavbarBrand>
+            {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick}/>}
+            <MDBCollapse isOpen={this.state.collapse} navbar>
+              <MDBNavbarNav right>
+                <MDBNavItem>
+                  <MDBNavLink to="/login" className="font-weight-bold text-warning">Iniciar Sesión</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/sign_up" className="font-weight-bold text-default">Registrate</MDBNavLink>
+                </MDBNavItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
+          </MDBContainer>
+        </MDBNavbar>
+      </header>
+    );
+  }
+}
+
+export default Header;
