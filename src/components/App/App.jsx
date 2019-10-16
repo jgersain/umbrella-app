fd20db65d143aad434b4350612cbf794e5e68ec8
jgersain <jhersein@gmail.com>
2019-10-16 18:36:28 -0500

    add main compoenent and define home page

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Layout from '../Layout';
+import Home from '../../pages/Home';
+
+const App = () => (
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
+);
+
+export default App;
