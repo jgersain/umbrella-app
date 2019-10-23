0b7e4041e557e1012d164c82e7129589b16318f5
Angel Martinez <fenixturbo@hotmail.com>
2019-10-22 23:29:55 -0500

    Adding filter in Model Users for GraphQL

--- api/schema.py
+++ api/schema.py
@@ -13,7 +13,7 @@ class UserType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     """ Definición de las respuestas a las consultas posibles """
-
+    user = graphene.Field(UserType, id=graphene.Int(), email=graphene.String(), name=graphene.String())
     # Se definen los posibles campos en las consultas
     all_users = graphene.List(UserType)  # allUsers
 
@@ -21,6 +21,20 @@ class Query(graphene.ObjectType):
     def resolve_all_users(self, info, **kwargs):
         # Responde con la lista de todos registros
         return User.objects.all()
+    
+    def resolve_user(self, info, **kwargs):
+        id = kwargs.get('id')
+        email = kwargs.get('email')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return User.objects.get(pk=id)
+        if email is not None:
+            return User.objects.get(email=email)
+        if name is not None:
+            return User.objects.get(name=name)
+        
+        return None
 
 class CreateUser(graphene.Mutation):
     """ Permite realizar la operación de crear en la tabla User """
@@ -28,12 +42,12 @@ class CreateUser(graphene.Mutation):
     class Arguments:
         """ Define los argumentos para crear una User """
         name = graphene.String(required=True)
-        email = graphene.String()
-        password = graphene.String()
-        confirmPassword = graphene.String()
-        
-    # El atributo usado para la respuesta de la mutación
-    user = graphene.Field(UserType)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+        confirmPassword = graphene.String(required=True)
+
+
+    response = graphene.Boolean()
 
     def mutate(self, info, name, email, password, confirmPassword):
         """ Se encarga de crear el nuevo Usuario """
@@ -44,9 +58,47 @@ class CreateUser(graphene.Mutation):
             password=password,
             )
             user.save()
+            response = True
+        else:
+            response = False
 
-        return CreateUser(user=user)
+        # False si las contraseñas son diferentes 
+        # True su las contraseñas coinciden    
+        return CreateUser(response=response)
 
+class UpdateUser(graphene.Mutation):
+    """ Permite realizar la operación de modificar en la tabla User """
+    class Arguments:
+        """ Define los argumentos para modificar un Usuario """
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        email = graphene.String()
+        password = graphene.String()
+
+    response = graphene.String()
+    def mutate(self, info, id, name=None, email=None, password=None):
+        """
+        Se encarga de modificar el Usuario identificado por el id.
+        """
+
+        try:
+            # Si existe se modifica
+            user = User.objects.get(pk=id)
+            # Si algunos de los atributos es proporcionado, entonces se
+            # actualiza
+            if name is not None:
+              user.name = name
+            if email is not None:
+              user.email = email
+            if password is not None:
+              user.password = password
+            
+            user.save()
+            response = "True"
+        except User.DoesNotExist:
+            response = "El usuario no existe"
+        # Se regresa una instancia de esta mutación
+        return UpdateUser(response=response)
 
 class DeleteUser(graphene.Mutation):
     """ Permite realizar la operación de eliminar en la tabla User """
@@ -69,49 +121,14 @@ class DeleteUser(graphene.Mutation):
         except User.DoesNotExist:
             # No existe el Usuario
             response = False
-        # Se regresa una instancia de esta mutación
+
         return DeleteUser(response=response)
 
-
-class UpdateUser(graphene.Mutation):
-    """ Permite realizar la operación de modificar en la tabla User """
-    class Arguments:
-        """ Define los argumentos para modificar un Usuario """
-        id = graphene.ID(required=True)
-        name = graphene.String()
-        email = graphene.String()
-        password = graphene.String()
-    # El campo regresado como respuesta de la mutación, en este caso se regres el modificada.
-    user = graphene.Field(UserType)
-
-    def mutate(self, info, id, name=None, email=None, password=None):
-        """
-        Se encarga de modificar la Zona identificada por el id.
-        """
-        try:
-            # Si la zona existe se modifica
-            user = User.objects.get(pk=id)
-            # Si algunos de los atributos es proporcionado, entonces se
-            # actualiza
-            if name is not None:
-              user.name = name
-            if email is not None:
-              user.email = email
-            if password is not None:
-              user.password = password
-            user.save()
-        except user.DoesNotExist:
-            # Si el Usuario no existe, se procesa la excepción
-            user = None
-        # Se regresa una instancia de esta mutación
-        return UpdateUser(user=user)
-
-
-class Mutaciones(graphene.ObjectType):
+class Mutations(graphene.ObjectType):
     createUser = CreateUser.Field()
     updateUser = UpdateUser.Field()
     deleteUser = DeleteUser.Field()
 
 
 # Se crea un esquema que hace uso de la clase Query
-schema = graphene.Schema(query=Query, mutation=Mutaciones)
+schema = graphene.Schema(query=Query, mutation=Mutations)
