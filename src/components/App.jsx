4d95992cb151dd1e9c99ea39f7e3f93dc5cb60dc
jgersain <jhersein@gmail.com>
2019-10-31 01:03:53 -0600

    setup apollo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { setContext } from 'apollo-link-context';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import Layout from './Layout';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Looking from '../pages/Looking';
 import NotFound from '../pages/NotFound';
+import { AUTH_TOKEN } from '../constants';
 
 // apollo setup
-const client = new ApolloClient({
-  uri: 'https://angelmartinez04.pythonanywhere.com/api/graphql'
+const httpLink = createHttpLink({
+  uri: 'https://angelmartinez04.pythonanywhere.com/api/graphql',
   // uri: 'http://localhost:8000/api/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 const App = () => (
